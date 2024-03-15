e1baa099b859976793f89132b55e314919624723
Aziz Developer <aamiruddinaziz06@gmail.com>
2024-03-15 09:33:05 +0700

    Aziz : menambahkan view

--- resources/views/landingPage.blade.php
+++ resources/views/landingPage.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Landing Page</title>
     </head>
     <body class="antialiased">
         
