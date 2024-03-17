a2486cf1316e8186485994d0c2ae02ca7dfc7179
Aziz Developer <aamiruddinaziz06@gmail.com>
2024-03-17 22:23:35 +0700

    Aziz : Menambahkan form

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -22,19 +22,75 @@
   </head>
   <body class="antialiased">
     <x-navbar/>
+    <div class="table-responsive-sm">
+      <div id="table">
+        <table class="table table-striped bg-danger bg-opacity-75">
+          <thead>
+            <tr class="border-bottom">
+              <th scope="col" class="text-center bg-danger bg-opacity-10">Hari</th>
+              <td scope="col" class="text-center position-absolute start-50 translate-middle-x bg-transparent"><strong>Jam Booking</strong></td>
+            </tr>
+          </thead>
+          <tbody class="table-group-divider">
+            <tr id="row_hari"></tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
-    <div id="table">
-      <table class="table bg-danger bg-opacity-75">
-        <thead>
-          <tr class="border-bottom">
-            <th scope="col" class="text-center bg-danger bg-opacity-10">Hari</th>
-            <td scope="col" class="text-center position-absolute start-50 translate-middle-x bg-transparent"><strong>Jam Booking</strong></td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr id="row_hari"></tr>
-        </tbody>
-      </table>
+    <h2 class="mt-5"><u>Booking Treatment</u></h2>
+  
+    <div class="container text-center p-5">
+      <div class="row justify-content-center ms-5 me-5">
+        <div class="col-4 border p-3 w-50">
+          <div class="p-4">
+            <h5 class="text-start">Pilih Treatment</h5>
+            <div class="container">
+              <div class="row">
+                <div class="col">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="Peeling" id="Peeling">
+                    <label class="form-check-label" for="Peeling">
+                      Peeling
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="Sliming" id="Sliming">
+                    <label class="form-check-label" for="Sliming">
+                      Sliming
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="Meso Cell" id="Meso Cell">
+                    <label class="form-check-label" for="Meso Cell">
+                      Meso Cell
+                    </label>
+                  </div>
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="Infuso Whitening" id="Infuso Whitening">
+                    <label class="form-check-label" for="Infuso Whitening">
+                      Infuso Whitening
+                    </label>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="Laser" id="Laser">
+                    <label class="form-check-label" for="Laser">
+                      Laser
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-4 border p-3 w-50">
+          <div class="p-4">
+            <h5 class="text-start">Pilih Jam</h5>
+          </div>
+        </div>
+      </div>
     </div>
 
     <script>
