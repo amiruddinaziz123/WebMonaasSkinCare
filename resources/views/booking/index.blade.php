ed205ce16643fbcc2ed25af050c80e7bbaabe914
Aziz Developer <aamiruddinaziz06@gmail.com>
2024-03-28 16:06:48 +0700

    Aziz : Edit edit dikit

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -101,9 +101,7 @@
     <script src="{{ asset('js/booking.js') }}"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script>
-      AOS.init();
-    </script>
+    <script>AOS.init();</script>
     <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
   </body>
