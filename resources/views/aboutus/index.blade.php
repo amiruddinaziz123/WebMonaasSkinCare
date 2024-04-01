95b9791c031062d3f3d0e3f65ff83cfcc137c5e6
Ridho Hafid <ridhohafid680@gmail.com>
2024-04-01 10:19:51 +0700

    nambah column belom bisa (nabil)

--- resources/views/aboutus/index.blade.php
+++ resources/views/aboutus/index.blade.php
@@ -14,11 +14,21 @@
         </div>
 
         <div class="container">
-          <div class="row">
+          <div class="row text-center align-items-center justify-content-center" style="margin-top: 90px">
             <h2>Hasil Maksimal, Harga Minimal</h2>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam temporibus aspernatur modi sit, sequi voluptates quam itaque minima repellendus. Ipsam repudiandae ullam voluptate nostrum quos sit praesentium, beatae in reprehenderit adipisci? Accusantium debitis exercitationem dolorum nihil, maxime praesentium sequi eaque, accusamus commodi quaerat quisquam odit obcaecati enim illum sint dignissimos necessitatibus veniam unde. Voluptatum, sit neque quaerat nemo nulla, modi quia ipsam accusantium tempore eum sint quidem accusamus. Cumque architecto illo amet facilis voluptatibus dolorem adipisci aspernatur, sint accusantium voluptas. Placeat voluptate, praesentium laboriosam officia quia laborum facere officiis molestiae magnam rerum reiciendis, nesciunt, ut enim sequi possimus soluta quo.</p>
+            <p class="mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam temporibus aspernatur modi sit, sequi voluptates quam itaque minima repellendus. Ipsam repudiandae ullam voluptate nostrum quos sit praesentium, beatae in reprehenderit adipisci? Accusantium debitis exercitationem dolorum nihil, maxime praesentium sequi eaque, accusamus commodi quaerat quisquam odit obcaecati enim illum sint dignissimos necessitatibus veniam unde. Voluptatum, sit neque quaerat nemo nulla, modi quia ipsam accusantium tempore eum sint quidem accusamus. Cumque architecto illo amet facilis voluptatibus dolorem adipisci aspernatur, sint accusantium voluptas. Placeat voluptate, praesentium laboriosam officia quia laborum facere officiis molestiae magnam rerum reiciendis, nesciunt, ut enim sequi possimus soluta quo.</p>
           </div>
+          <hr style="margin-top: 70px"> 
+          <div class="col-md-6 ">
+              <h2>Mengapa Harus Monaas</h2>
+              {{-- <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis enim voluptatem porro non optio? Architecto, officiis. Eius assumenda aliquid eos recusandae ut animi fuga dicta! Aliquam quidem itaque similique totam!
+              </p> --}}
+          </div>
+          <div class="col-md-6">
+             <img src="/img/ken.png" alt="" class="w-25"> 
+          </div>
+
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
