4ad3f84d53be75cf61ac1b28986fdb626636da2a
Nabil <nabilyamashita@gmail.com>
2024-04-08 00:18:06 -0700

    database

---
+++ database/migrations/2024_04_08_062940_create_abouts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('abouts', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('intro');
+            $table->string('reason');
+            $table->string('icon1');
+            $table->string('tittle_icon1');
+            $table->string('isi_icon1');
+            $table->string('icon2');
+            $table->string('tittle_icon2');
+            $table->string('isi_icon2');
+            $table->string('icon3');
+            $table->string('tittle_icon3');
+            $table->string('isi_icon3');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('abouts');
+    }
+};
