c35b44bc1148a01a059546d3c370393c56df5035
Ridho Hafid <ridhohafid680@gmail.com>
2024-04-21 22:30:00 +0700

    compo admin

---
+++ resources/views/components/menu-admin.blade.php
@@ -0,0 +1,38 @@
+<header>
+    <nav
+    class="navbar navbar-expand-lg navbar-dark shadow fixed-bottom"
+    style="background: linear-gradient(90deg, rgba(24,24,24,1) 49%, rgba(43,149,112,1) 100%);"
+  >
+    <div class="container">
+      <a class="navbar-brand harry fs-1" href="../index.html">Harry Potter</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto fs-6 fw-bold">
+          <li class="nav-item">
+            <a class="nav-link" href="../cerita1/index.html"
+              >Cerita 1</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Cerita 2</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../cerita3/index.html">Cerita 3</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../cerita4/index.html">Cerita 4</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
