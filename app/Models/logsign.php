cdd0be43305ba2f3ae7adf53cfeac8f366d60a9a
Aziz Developer <aamiruddinaziz06@gmail.com>
2024-04-22 05:42:58 +0700

    Aziz : update ridho

--- app/Models/logsign.php
+++ app/Models/logsign.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class navbar extends Model
+class logsign extends Model
 {
     use HasFactory;
     /**
