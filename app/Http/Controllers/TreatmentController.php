46af3b217fbeb11132e9d59c75e961e5560f9348
AdindaAL21 <adindaal2101@gmail.com>
2024-04-21 14:05:46 +0700

    dinda

---
+++ app/Http/Controllers/TreatmentController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use App\Models\Treatment;
+
+class TreatmentController extends Controller
+{
+    public function admin() {
+        $treatments = Treatment::all();
+        return view('treatment/treatment_admin/AdmTreatment', compact('treatments'));
+    }
+
+    public function create() {
+        return view('treatment/treatment_admin/TambahTreatment');
+    }
+
+
+    public function store(Request $request) {
+        // Validasi data
+        $request->validate([
+            'foto_treatment' => 'image|mimes:jpeg,png,jpg',
+            'slug_link' => 'unique:products,slug_link',
+        ]);
+
+        if ($request->hasFile('foto_treatment')) {
+            $imagePath = $request->file('foto_treatment')->store('images');
+            $imageUrl = asset('storage/' . $imagePath);
+        } else {
+            $imageUrl = null;
+        }
+
+        $slug = Str::slug($request->slug_link, '_');
+
+        $treatments = new Treatment([
+            'foto_treatment' => $imageUrl,
+            'nama_treatment' => $request->input('nama_treatment'),
+            'description_treatment' => $request->input('description_treatment'),
+            'status_publish' => $request->input('status_publish'),
+            'slug_link' => $slug,
+        ]);
+
+        $treatments->save();
+
+        return redirect()->route('Admin.admin')->with(['success' => 'Berhasil menambahkan treatment !']);
+    }
+
+}
---
+++ resources/views/treatment/treatment_admin/AdmTreatment.blade.php
@@ -0,0 +1,161 @@
+@extends('treatment/layout')
+
+@section('content')
+
+<div class="col-12">
+    <a href="{{ route('Admin.admin') }}" class="btn btn-success btn-sm">Produk</a> >>
+    <a href="{{ route('Admin.history') }}" class="btn btn-dark btn-sm">History</a>
+</div>
+<div class="col-12 mt-4">
+    <a href="{{ route('Admin.create') }}" class="btn btn-primary btn-sm"><i class="bi bi-square-plus"></i></i> Tambah</a>
+    </div>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                  <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="{{ asset('img/gambar1.jpg') }}" class="d-block" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="{{ asset('img/gambar1.jpg') }}" class="d-block" alt="...">
+                  </div>
+                  <div class="carousel-item">
+                    <img src="{{ asset('img/gambar1.jpg') }}" class="d-block" alt="...">
+                  </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
+              </div>
+
+<br>
+<br>
+<br>
+
+<!-- CARD -->
+
+<h3>Facial Treatment</h3>
+
+<div class="row">
+            <div class="col">
+            <div class="card my-3">
+                <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Laser Treatment</h5>
+                    <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                    <a href="#" class="btn">Booking</a>
+                </div>
+            </div>
+        </div>
+
+            <div class="col">
+                <div class="card my-3">
+                    <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Laser Treatment</h5>
+                        <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                        <a href="#" class="btn">Booking</a>
+                    </div>
+                </div>
+                </div>
+
+
+                <div class="col">
+                    <div class="card my-3">
+                        <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Laser Treatment</h5>
+                            <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                            <a href="#" class="btn">Booking</a>
+                        </div>
+                    </div>
+                    </div>
+</div>
+
+<br><br>
+
+<h3>Facial Treatment</h3>
+<div class="row">
+            <div class="col">
+            <div class="card my-3">
+                <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Laser Treatment</h5>
+                    <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                    <a href="#" class="btn">Booking</a>
+                </div>
+            </div>
+        </div>
+
+            <div class="col">
+                <div class="card my-3">
+                    <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Laser Treatment</h5>
+                        <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                        <a href="#" class="btn">Booking</a>
+                    </div>
+                </div>
+                </div>
+
+
+                <div class="col">
+                    <div class="card my-3">
+                        <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Laser Treatment</h5>
+                            <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                            <a href="#" class="btn">Booking</a>
+                        </div>
+                    </div>
+                    </div>
+</div>
+
+<h3>Facial Treatment</h3>
+<div class="row">
+            <div class="col">
+            <div class="card my-3">
+                <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Laser Treatment</h5>
+                    <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                    <a href="#" class="btn">Booking</a>
+                </div>
+            </div>
+        </div>
+
+            <div class="col">
+                <div class="card my-3">
+                    <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Laser Treatment</h5>
+                        <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                        <a href="#" class="btn">Booking</a>
+                    </div>
+                </div>
+                </div>
+
+
+                <div class="col">
+                    <div class="card my-3">
+                        <img src="{{ asset('img/gambar1.jpg') }}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Laser Treatment</h5>
+                            <p class="card-text">Perawatan untuk mengencangkan kulit wajah dan mengurangi keriput di wajah.</p>
+                            <a href="#" class="btn">Booking</a>
+                        </div>
+                    </div>
+                    </div>
+</div>
+
+
+@endsection
