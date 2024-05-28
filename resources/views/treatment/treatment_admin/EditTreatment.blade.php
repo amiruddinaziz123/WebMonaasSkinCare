a08756d77f066bb43e04b29697d9b89c1ecca8a4
Ridho Hafid <ridhohafid680@gmail.com>
2024-05-29 00:06:54 +0700

    treatment admin

--- resources/views/treatment/treatment_admin/EditTreatment.blade.php
+++ resources/views/treatment/treatment_admin/EditTreatment.blade.php
@@ -2,105 +2,123 @@
 
 @section('content')
 
+{{-- DIV PEMBUNGKUS AGAR RESPONSIF --}}
+<div class="content-wrapper">
 
-<div class="container">
-
-        <form method="POST" action="{{ route('treatment_admin.update', $treatments->slug_link) }}" enctype="multipart/form-data">
-            @csrf
-            @method('PUT')
-            <div class="mb-3 row mt-5">
-                <label for="foto" class="col-sm-2 col-form-label">Foto Treatment</label>
-                <div class="col-sm-10">
-                    @if ($treatments->foto_treatment)
-                        <img src="{{ asset('storage/images/' . $treatments->foto_treatment)}}" alt="Monaas Skincare" class="img-fluid" style="width: 500px; margin-bottom: 15px;">
-                    @else
-                        <p>Tidak ada gambar yang tersedia</p>
-                 @endif
-                    <input required type="file" name="foto_treatment" class="form-control @error('foto_treatment') is-invalid @enderror" id="foto"  value="{{$treatments->foto_treatment}}">
-                    @error('foto_treatment')
-                    <div class="alert alert-danger mt-2">
-                        {{ $message }}
-                    </div>
-                    @enderror
+    <!-- HEADER DONTENT -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Edit Treatment</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Edit Treatment</li> </ol>
                 </div>
             </div>
-            <div class="mb-3 row mt-3">
-                <label for="nmTreatment" class="col-sm-2 col-form-label">Nama Treatment</label>
-                <div class="col-sm-10">
-                    <input required type="text" name="nama_treatment" class="form-control @error('nama_treatment') is-invalid @enderror" id="nmTreatment" placeholder="ex: Laser" value="{{$treatments->nama_treatment}}">
-                    @error('nama_treatment')
-                    <div class="alert alert-danger mt-2">
+        </div>
+    </div>
+
+    {{-- ISI KONTEN --}}
+    <section class="content">
+        <div class="container-fluid">
+
+            <form method="POST" action="{{ route('treatment_admin.update', $treatments->slug_link) }}" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="mb-3 row mt-5">
+                    <label for="foto" class="col-sm-2 col-form-label">Foto Treatment</label>
+                    <div class="col-sm-10">
+                        @if ($treatments->foto_treatment)
+                            <img src="{{ asset('storage/images/' . $treatments->foto_treatment)}}" alt="Monaas Skincare" class="img-fluid" style="width: 500px; margin-bottom: 15px;">
+                        @else
+                            <p>Tidak ada gambar yang tersedia</p>
+                     @endif
+                        <input required type="file" name="foto_treatment" class="form-control @error('foto_treatment') is-invalid @enderror" id="foto"  value="{{$treatments->foto_treatment}}">
+                        @error('foto_treatment')
+                        <div class="alert alert-danger mt-2">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="mb-3 row mt-3">
+                    <label for="nmTreatment" class="col-sm-2 col-form-label">Nama Treatment</label>
+                    <div class="col-sm-10">
+                        <input required type="text" name="nama_treatment" class="form-control @error('nama_treatment') is-invalid @enderror" id="nmTreatment" placeholder="ex: Laser" value="{{$treatments->nama_treatment}}">
+                        @error('nama_treatment')
+                        <div class="alert alert-danger mt-2">
+                            {{ $message }}
+                        </div>
+                        @enderror
+                    </div>
+                </div>
+    
+                <div class="form-group mb-3 row">
+                    <label for="harga" class="font-weight-bold col-sm-2 col-form-label">Harga</label>
+                    <div class="col-sm-10">
+                        <input type="number" class="form-control @error('harga_treatment') is-invalid @enderror" name="harga_treatment" value="{{$treatments->harga_treatment}}" placeholder="Contoh: 100">
+                        @error('harga_treatment')
+                        <div class="alert alert-danger mt-2">
                         {{ $message }}
-                    </div>
+                        </div>
                     @enderror
-                </div>
-            </div>
-
-            <div class="form-group mb-3 row">
-                <label for="harga" class="font-weight-bold col-sm-2 col-form-label">Harga</label>
-                <div class="col-sm-10">
-                    <input type="number" class="form-control @error('harga_treatment') is-invalid @enderror" name="harga_treatment" value="{{$treatments->harga_treatment}}" placeholder="Contoh: 100">
-                    @error('harga_treatment')
-                    <div class="alert alert-danger mt-2">
-                    {{ $message }}
                     </div>
-                @enderror
                 </div>
-            </div>
-
-            {{-- <div class="form-group mb-3 row">
-                <label for="status" class="font-weight-bold col-sm-2 col-form-label">Status Publish</label>
-                <div class="col-sm-10">
-                    <select required name="status_publish" id="status" class="form-control @error('status_publish') is-invalid @enderror">
-                        <option selected>{{$treatments->status_publish}}</option>
-                        <option value="Draft">Draft</option>
-                        <option value="Publish">Publish</option>
-                    </select>
-                    @error('status_publish')
-                    <div class="alert alert-danger mt-2">
-                        {{ $message }}
+    
+                {{-- <div class="form-group mb-3 row">
+                    <label for="status" class="font-weight-bold col-sm-2 col-form-label">Status Publish</label>
+                    <div class="col-sm-10">
+                        <select required name="status_publish" id="status" class="form-control @error('status_publish') is-invalid @enderror">
+                            <option selected>{{$treatments->status_publish}}</option>
+                            <option value="Draft">Draft</option>
+                            <option value="Publish">Publish</option>
+                        </select>
+                        @error('status_publish')
+                        <div class="alert alert-danger mt-2">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
-                    @enderror
-                </div>
-            </div> --}}
-
-            <div class="mb-3 row">
-                <label for="deskripsi" class="col-sm-2 col-form-label">Deskripsi</label>
-                <div data-mdb-input-init class="col-sm-10 form-outline">
-                    <textarea required type="text" name="description_treatment" class="form-control @error('description_treatment') is-invalid @enderror" id="deskripsi" rows="4">{{$treatments->description_treatment}}</textarea>
-                    @error('description_treatment')
-                    <div class="alert alert-danger mt-2">
-                        {{ $message }}
+                </div> --}}
+    
+                <div class="mb-3 row">
+                    <label for="deskripsi" class="col-sm-2 col-form-label">Deskripsi</label>
+                    <div data-mdb-input-init class="col-sm-10 form-outline">
+                        <textarea required type="text" name="description_treatment" class="form-control @error('description_treatment') is-invalid @enderror" id="deskripsi" rows="4">{{$treatments->description_treatment}}</textarea>
+                        @error('description_treatment')
+                        <div class="alert alert-danger mt-2">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
-                    @enderror
                 </div>
-            </div>
-
-            {{-- <div class="mb-3 row">
-                <label for="deskripsi" class="col-sm-2 col-form-label">Deskripsi</label>
-                <div class="col-sm-10">
-                    <input required type="text" name="description_treatment" class="form-control @error('description_treatment') is-invalid @enderror" id="deskripsi" value="{{$treatments->description_treatment}}">
-                    @error('description_treatment')
-                    <div class="alert alert-danger mt-2">
-                        {{ $message }}
+    
+                {{-- <div class="mb-3 row">
+                    <label for="deskripsi" class="col-sm-2 col-form-label">Deskripsi</label>
+                    <div class="col-sm-10">
+                        <input required type="text" name="description_treatment" class="form-control @error('description_treatment') is-invalid @enderror" id="deskripsi" value="{{$treatments->description_treatment}}">
+                        @error('description_treatment')
+                        <div class="alert alert-danger mt-2">
+                            {{ $message }}
+                        </div>
+                        @enderror
                     </div>
-                    @enderror
+                </div> --}}
+    
+                <div class="row mb-3 mt-5">
+                    <div class="col pinggir">
+                            <button type="submit" name="aksi" value="edit" class="btn btn-success"><i class="bi bi-save"></i> Simpan</button>
+                            <button type="reset" name="aksi" value="reset" class="btn btn-secondary"><i class="bi bi-repeat"></i> Reset </button>
+                            <a href=" {{ route('treatment_admin.admin') }} " type="button" class="btn btn-danger"><i class="bi bi-arrow-left-square"></i> Batal </a>
+                    </div>
                 </div>
-            </div> --}}
-
-            <div class="row mb-3 mt-5">
-                <div class="col pinggir">
-                        <button type="submit" name="aksi" value="edit" class="btn btn-success"><i class="bi bi-save"></i> Simpan</button>
-                        <button type="reset" name="aksi" value="reset" class="btn btn-secondary"><i class="bi bi-repeat"></i> Reset </button>
-                        <a href=" {{ route('treatment_admin.admin') }} " type="button" class="btn btn-danger"><i class="bi bi-arrow-left-square"></i> Batal </a>
-                </div>
-            </div>
-
-        </form>
-
-        <br><br><br>
-
-
+    
+            </form>
+        </div>
+    </section>
 </div>
 
 <style>
--- resources/views/treatment/treatment_admin/HistoryTreatment.blade.php
+++ resources/views/treatment/treatment_admin/HistoryTreatment.blade.php
@@ -1,67 +1,90 @@
-@extends('treatment/layout')
+@extends('layouts.admin')
 
 @section('content')
 
-<div class="row justify-content-center text-center">
-    <div class="col-12 mt-4 mb-5">
-        <h1>OUR TREATMENT</h1>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Histori Treatment</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Histori Treatment</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <div class="col-12">
-        <table id="example" class="table table-striped" style="width:100%">
-            <thead>
-                <tr>
-                    <th data-priority="1">No</th>
-                    <th>Foto Treatment</th>
-                    <th data-priority="1">Nama Treatment</th>
-                    <th data-priority="1">Deskripsi</th>
-                    <th data-priority="1">Harga</th>
-                    <th data-priority="1">Aksi</th>
-                </tr>
-            </thead>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="col-12">
+                <a href="{{ route('treatment_admin.admin') }}" class="btn btn-primary">
+                    <i class="bi bi-people"></i> Treatment
+                </a>
+            </div>
 
-            <tbody>
-                @foreach ($treatments as $no => $treat)
-
-                <tr>
-                    <td>{{++$no}}</td>
-                    <td> <img src="{{ asset('storage/images/' . $treat->foto_treatment)}}" class="card-img-top" alt="..." style="width: 160px;"> </td>
-                    <td> {{ $treat->nama_treatment}} </td>
-                    <td> {{ $treat->description_treatment }} </td>
-                    <td> {{ $treat->harga_treatment}}</td>
-
-                    <td>
-                        <form onsubmit="return confirm('Yakin ingin mempublish ini ?');" action="{{ route('treatment_admin.restore', ['slug_link' => $treat->slug_link]) }}" method="POST">
-                            @csrf
-                            @method('POST')
-                            <button type="submit" class="btn btn-primary btn-sm mt-2">
-                                <i class="bi bi-box-arrow-left"></i> Restore
-                            </button>
-                        </form>
-
-                        <form onsubmit="return confirm('Yakin ingin menghapus ini ?');" action="{{ route('treatment_admin.deletePermanent', $treat->id) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-outline-danger btn-sm mt-2">
-                                <i class="bi bi-trash3"></i> Delete Permanent
-                            </button>
-                        </form>
-                    </td>
-
-                @endforeach
-            </tbody>
-
-            <tfoot>
-                <tr>
-                    <th>No</th>
-                    <th>Foto Treatment</th>
-                    <th>Nama Treatment</th>
-                    <th>Deskripsi</th>
-                    <th>Aksi</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
+            <div class="col-12">
+                <table id="example" class="table table-dark" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th data-priority="1">No</th>
+                            <th>Foto Treatment</th>
+                            <th data-priority="1">Nama Treatment</th>
+                            <th data-priority="1">Deskripsi</th>
+                            <th data-priority="1">Harga</th>
+                            <th data-priority="1">Aksi</th>
+                        </tr>
+                    </thead>
+        
+                    <tbody>
+                        @foreach ($treatments as $no => $treat)
+        
+                        <tr>
+                            <td>{{++$no}}</td>
+                            <td> <img src="{{ asset('storage/images/' . $treat->foto_treatment)}}" class="card-img-top" alt="..." style="width: 160px;"> </td>
+                            <td> {{ $treat->nama_treatment}} </td>
+                            <td> {{ $treat->description_treatment }} </td>
+                            <td> {{ $treat->harga_treatment}}</td>
+        
+                            <td>
+                                <form onsubmit="return confirm('Yakin ingin mempublish ini ?');" action="{{ route('treatment_admin.restore', ['slug_link' => $treat->slug_link]) }}" method="POST">
+                                    @csrf
+                                    @method('POST')
+                                    <button type="submit" class="btn btn-primary btn-sm mt-2">
+                                        <i class="bi bi-box-arrow-left"></i> Restore
+                                    </button>
+                                </form>
+        
+                                <form onsubmit="return confirm('Yakin ingin menghapus ini ?');" action="{{ route('treatment_admin.deletePermanent', $treat->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-outline-danger btn-sm mt-2">
+                                        <i class="bi bi-trash3"></i> Delete Permanent
+                                    </button>
+                                </form>
+                            </td>
+        
+                        @endforeach
+                    </tbody>
+        
+                    <tfoot>
+                        <tr>
+                            <th>No</th>
+                            <th>Foto Treatment</th>
+                            <th>Nama Treatment</th>
+                            <th>Deskripsi</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </section>
 </div>
 
 @endsection
