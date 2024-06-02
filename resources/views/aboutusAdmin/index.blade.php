1967b74bc869fd7fecd25397082fb42ed1b71943
Ridho Hafid <ridhohafid680@gmail.com>
2024-06-02 11:43:14 +0700

    ridho: nullable aboutus admin

--- resources/views/aboutusAdmin/index.blade.php
+++ resources/views/aboutusAdmin/index.blade.php
@@ -30,7 +30,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>about us</title>
+    <title>About Us</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/css/aboutus.css">
 </head>
@@ -46,7 +46,7 @@
             <div class="text-center align-items-center justify-content-center">
               <h2 style="color: black">Hasil <span>Maksimal</span>, Harga <span>Minimal</span></h2>
               <p class="mt-4" style="color: black">
-                {{ $abouts->intro }}
+                {{ $abouts->intro ?? 'Default introduction text or placeholder.' }}
               </p>
             </div>
 
@@ -55,35 +55,33 @@
             <div class="col-md-6 mt-5 mb-5">
               <h2 style="color: black">Mengapa Harus <span>Monaas</span></h2>
               <p style="color: black">
-                {{ $abouts->reason }}  
+                {{ $abouts->reason ?? 'Default reason text or placeholder.' }}  
               </p>
             </div>
 
             <div class="col-md-6 mt-5 mb-5">
-              <img src="/img/{{ $abouts->image }}" alt="" class="w-100"> 
+              <img src="{{ isset($abouts) && $abouts->image ? '/img/' . $abouts->image : '/img/default-image.jpg' }}" alt="" class="w-100"> 
             </div>
 
             <div class="col-md-4 mt-5 mb-5 text-center align-items-center" style="color: black">
-              <img src="/img/{{ $abouts->icon1 }}" alt="">
-              <h5>{{ $abouts->tittle_icon1 }}</h5>
-              <P>{{ $abouts->isi_icon1 }}</P>
+              <img src="{{ isset($abouts) && $abouts->icon1 ? '/img/' . $abouts->icon1 : '/img/default-icon1.jpg' }}" alt="">
+              <h5>{{ $abouts->tittle_icon1 ?? 'Default Icon 1 Title' }}</h5>
+              <P>{{ $abouts->isi_icon1 ?? 'Default description for Icon 1.' }}</P>
             </div>
 
             <div class="col-md-4 mt-5 mb-5 text-center align-items-center" style="color: black">
-              <img src="/img/{{ $abouts->icon2 }}" alt="">
-              <H5>{{ $abouts->tittle_icon2 }}</H5>
-              <p>{{ $abouts->isi_icon2 }}</p>
+              <img src="{{ isset($abouts) && $abouts->icon2 ? '/img/' . $abouts->icon2 : '/img/default-icon2.jpg' }}" alt="">
+              <H5>{{ $abouts->tittle_icon2 ?? 'Default Icon 2 Title' }}</H5>
+              <p>{{ $abouts->isi_icon2 ?? 'Default description for Icon 2.' }}</p>
             </div>
 
             <div class="col-md-4 mt-5 mb-5 text-center align-items-center" style="color: black">
-              <img src="/img/{{ $abouts->icon3 }}" alt="">
-              <h5>{{ $abouts->tittle_icon3 }}</h5>
-              <P>{{ $abouts->isi_icon3 }}</P>
+              <img src="{{ isset($abouts) && $abouts->icon3 ? '/img/' . $abouts->icon3 : '/img/default-icon3.jpg' }}" alt="">
+              <h5>{{ $abouts->tittle_icon3 ?? 'Default Icon 3 Title' }}</h5>
+              <P>{{ $abouts->isi_icon3 ?? 'Default description for Icon 3.' }}</P>
             </div>
             
           </div>
-
-
         </div>
     </div>
 
@@ -94,66 +92,66 @@
       @csrf
       <div class="form-group mt-4">
         <label for="text">Intro</label>
-        <textarea class="form-control" id="text" name="intro" rows="3">{{ $abouts->intro }}</textarea>
+        <textarea class="form-control" id="text" name="intro" rows="3">{{ $abouts->intro ?? '' }}</textarea>
      </div>
   
      <div class="form-group mt-4">
       <label for="text">Reason</label>
-      <textarea class="form-control" id="text" name="reason" rows="3">{{ $abouts->intro }}</textarea>
+      <textarea class="form-control" id="text" name="reason" rows="3">{{ $abouts->reason ?? '' }}</textarea>
    </div>
   
    <div class="form-group mt-4">
     <label for="text" class="form-label d-block">Image</label>
-    <label for="formFile" class="form-label mb-3"><img src="/img/{{ $abouts->image }}" alt="" style="width: 200px"></label>
+    <label for="formFile" class="form-label mb-3"><img src="{{ isset($abouts) && $abouts->image ? '/img/' . $abouts->image : '/img/default-image.jpg' }}" alt="" style="width: 200px"></label>
     <input class="form-control" type="file" id="formFile" name="image">
   </div>
   
    <div class="form-group mt-4">
     <label for="text" class="form-label d-block">Icon 1</label>
-    <label for="formFile" class="form-label mb-3"><img src="/img/{{ $abouts->icon1 }}" alt="" style="width: 50px"></label>
+    <label for="formFile" class="form-label mb-3"><img src="{{ isset($abouts) && $abouts->icon1 ? '/img/' . $abouts->icon1 : '/img/default-icon1.jpg' }}" alt="" style="width: 50px"></label>
     <input class="form-control" type="file" id="formFile" name="icon1">
   </div>
   
   <div class="form-group mt-4">
     <label for="exampleFormControlInput1" class="form-label">Judul Icon 1</label>
-    <input type="text" class="form-control" id="exampleFormControlInput1" value="{{ $abouts->tittle_icon1 }}" name="tittle_icon1">
+    <input type="text" class="form-control" id="exampleFormControlInput1" value="{{ $abouts->tittle_icon1 ?? '' }}" name="tittle_icon1">
   </div>
   
   <div class="form-group mt-4">
     <label for="text">Deskripsi Icon 1</label>
-    <textarea class="form-control" id="text" name="isi_icon1" rows="3">{{ $abouts->isi_icon1 }}</textarea>
+    <textarea class="form-control" id="text" name="isi_icon1" rows="3">{{ $abouts->isi_icon1 ?? '' }}</textarea>
   </div>
   
   <div class="form-group mt-4">
     <label for="text" class="form-label d-block">Icon 2</label>
-    <label for="formFile" class="form-label mb-3"><img src="/img/{{ $abouts->icon2 }}" alt="" style="width: 50px"></label>
+    <label for="formFile" class="form-label mb-3"><img src="{{ isset($abouts) && $abouts->icon2 ? '/img/' . $abouts->icon2 : '/img/default-icon2.jpg' }}" alt="" style="width: 50px"></label>
     <input class="form-control" type="file" id="formFile" name="icon2">
   </div>
   
   <div class="form-group mt-4">
     <label for="exampleFormControlInput1" class="form-label">Judul Icon 2</label>
-    <input type="text" class="form-control" id="exampleFormControlInput1" value="{{ $abouts->tittle_icon2 }}" name="tittle_icon2">
+    <input type="text" class="form-control" id="exampleFormControlInput1" value="{{ $abouts->tittle_icon2 ?? '' }}" name="tittle_icon2">
   </div>
   
   <div class="form-group mt-4">
     <label for="text">Deskripsi Icon 2</label>
-    <textarea class="form-control" id="text" name="isi_icon2" rows="3">{{ $abouts->isi_icon2 }}</textarea>
+    <textarea class="form-control" id="text" name="isi_icon2" rows="3">{{ $abouts->isi_icon2 ?? '' }}</textarea>
   </div>
   
   <div class="form-group mt-4">
     <label for="text" class="form-label d-block">Icon 3</label>
-    <label for="formFile" class="form-label mb-3"><img src="/img/{{ $abouts->icon3 }}" alt="" style="width: 50px"></label>
+    <label for="formFile" class="form-label mb-3"><img src="{{ isset($abouts) && $abouts->icon3 ? '/img/' . $abouts->icon3 : '/img/default-icon3.jpg' }}" alt="" style="width: 50px"></label>
     <input class="form-control" type="file" id="formFile" name="icon3">
   </div>
   
   <div class="form-group mt-4">
     <label for="exampleFormControlInput1" class="form-label">Judul icon 3</label>
-    <input type="text" class="form-control" id="exampleFormControlInput1" value="{{ $abouts->tittle_icon3 }}" name="tittle_icon3">
+    <input type="text" class="form-control" id="exampleFormControlInput1" value="{{ $abouts->tittle_icon3 ?? '' }}" name="tittle_icon3">
   </div>
   
   <div class="form-group mt-4">
     <label for="text">Deskripsi icon 3</label>
-    <textarea class="form-control" id="text" name="isi_icon3" rows="3">{{ $abouts->isi_icon3 }}</textarea>
+    <textarea class="form-control" id="text" name="isi_icon3" rows="3">{{ $abouts->isi_icon3 ?? '' }}</textarea>
   </div>
 
   <div class="row justify-content-center">
